7d2c8959df5a7ebfa083f9f6e271d5afa7c0311a
IMDxD <nestor969@mail.ru>
2020-10-05 11:19:18 +0300

    fix back memory in hw4A

--- task_4/a.cpp
+++ task_4/a.cpp
@@ -33,11 +33,7 @@ class MinStack {
   void pop() {
     Node *prev_head_pointer = head_pointer;
     head_pointer = (*prev_head_pointer).next_node_pointer;
-    Node *new_min_node_pointer = (*prev_head_pointer).min_pointer;
-    if (new_min_node_pointer != min_node_pointer){
-      free(min_node_pointer);
-      min_node_pointer = new_min_node_pointer;
-    }
+    min_node_pointer = (*prev_head_pointer).min_pointer;
     free(prev_head_pointer);
   }
   
