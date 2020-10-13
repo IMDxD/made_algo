ca5a86653c7bde5c1b7c68d6cdc1b3e1ea2988cb
IMDxD <nestor969@mail.ru>
2020-10-13 15:42:55 +0300

    fix task_5a

--- task_5/a.cpp
+++ task_5/a.cpp
@@ -1,3 +1,4 @@
+#include "algorithm"
 #include "iostream"
 #include "string"
 #include "vector"
@@ -8,14 +9,13 @@ using std::string;
 using std::swap;
 using std::vector;
 
-const size_t OPER_CNT = 1'000'000;
-const unsigned A = 7;
-const unsigned P = 2'000'003;
+const size_t INITIAL_SIZE = 1024;
+const size_t MAX_SIZE = 2'000'002;
 
 unsigned modulo(int value, unsigned m) {
   int mod = value % (int) m;
   if (mod < 0) {
-    mod += m;
+    mod += static_cast<int>(m);
   }
   return mod;
 }
@@ -23,73 +23,89 @@ unsigned modulo(int value, unsigned m) {
 class Set {
  private:
 
-  vector<int> array;
-  unsigned p;
-  unsigned a;
+  vector<int>* array;
+  size_t size_;
+  const unsigned P = 2'000'003;
+  const unsigned A = 7;
   int null_value = INT32_MAX;
 
-  int hash_function(int value) const {
-    return modulo(value * this->a, this->p) % this->array.size();
+  size_t hash_function(int value) const {
+    return modulo(static_cast<int>(value * this->A), this->P) % this->array->size();
+  }
+
+  size_t find_index(int value){
+    size_t index = this->hash_function(value);
+    while (!((*this->array)[index] == null_value || (*this->array)[index] == value)) {
+      index = (index + 1) % this->array->size();
+    }
+    return index;
+  }
+
+  void rehash(size_t new_size){
+    vector<int>* old_array = this->array;
+    this->array = new vector<int>(new_size, INT32_MAX);
+    this->size_ = 0;
+    for (auto el: *old_array){
+      if (el != this->null_value){
+        this->put(el);
+      }
+    }
+    delete old_array;
+  }
+
+  void restore_hash(size_t index){
+    size_t next_index = (index + 1) % this->array->size();
+    size_t swap_index = index;
+    while ((*this->array)[next_index] != this->null_value) {
+      size_t next_hash = hash_function((*this->array)[next_index]);
+      if (next_hash > next_index || next_hash <= index) {
+        swap((*this->array)[swap_index], (*this->array)[next_index]);
+        swap_index = next_index;
+      }
+      ++next_index;
+    }
   }
 
  public:
-  Set(size_t new_size, int new_a, unsigned new_p) {
-    this->array = vector<int>(new_size * 2, this->null_value);
-    this->a = new_a;
-    this->p = new_p;
-  }
+  Set(): array(new vector<int>(INITIAL_SIZE, INT32_MAX)), size_(0) {};
 
   bool contains(int value) {
-    size_t index = this->hash_function(value);
-    while (this->array[index] != this->null_value) {
-      if (this->array[index] == value) {
-        return true;
-      } else {
-        index = (index + 1) % this->array.size();
-      }
-    }
-    return false;
+    size_t index = this->find_index(value);
+    return (*this->array)[index] == value;
   }
 
   void put(int value) {
-    size_t index = this->hash_function(value);
-    while (!(this->array[index] == null_value || this->array[index] == value)) {
-      index = (index + 1) % this->array.size();
+    if (this->size_ == this->array->size() /2){
+      size_t new_size = std::max(this->array->size() * 2, MAX_SIZE);
+      this->rehash(new_size);
     }
-    if (this->array[index] != value) {
-      this->array[index] = value;
+    size_t index = this->find_index(value);
+    if ((*this->array)[index] != value) {
+      (*this->array)[index] = value;
+      ++this->size_;
     }
   }
 
   void remove(int value) {
     size_t index = this->hash_function(value);
     size_t step = 0;
-    while (this->array[index] != this->null_value && step < this->array.size()) {
-      if (this->array[index] == value) {
-        this->array[index] = this->null_value;
-        size_t next_index = (index + 1) % this->array.size();
-        size_t swap_index = index;
-        while (this->array[next_index] != this->null_value) {
-          int next_hash = hash_function(this->array[next_index]);
-          if (next_hash > next_index || next_hash <= index) {
-            swap(this->array[swap_index], this->array[next_index]);
-            swap_index = next_index;
-          }
-          ++next_index;
-        }
+    while ((*this->array)[index] != this->null_value && step < this->array->size()) {
+      if ((*this->array)[index] == value) {
+        (*this->array)[index] = this->null_value;
+        restore_hash(index);
+        --this->size_;
         break;
       }
-      index = (index + 1) % this->array.size();
+      index = (index + 1) % this->array->size();
       ++step;
     }
-
   }
 };
 
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  Set hashset = Set(OPER_CNT, A, P);
+  Set hashset = Set();
   string oper;
   int value;
   while (cin >> oper >> value) {
