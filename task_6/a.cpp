3b4ac5b26c8165b25bc36a95f008f9d9f2691dd5
IMDxD <nestor969@mail.ru>
2020-10-17 19:45:35 +0300

    fix task6 a.cpp codestyle

--- task_6/a.cpp
+++ task_6/a.cpp
@@ -8,8 +8,7 @@ using std::vector;
 using std::min;
 using std::max;
 
-
-int main(){
+int main() {
   size_t column_count;
   size_t jump_range;
   cin >> column_count >> jump_range;
@@ -17,14 +16,14 @@ int main(){
   vector<int> result_cost(column_count, INT32_MIN + 1);
   result_cost[0] = 0;
   vector<size_t> jumped_columns(column_count, 0);
-  for (size_t i = 1; i < column_count - 1; ++i){
+  for (size_t i = 1; i < column_count - 1; ++i) {
     cin >> column_cost[i];
   }
-  for (size_t i = 1; i < column_count; ++i){
+  for (size_t i = 1; i < column_count; ++i) {
     size_t jumps_back = min(jump_range, i);
     size_t column = i;
-    for (size_t j = 1; j <= jumps_back; ++j){
-      if (result_cost[i] < result_cost[i - j]){
+    for (size_t j = 1; j <= jumps_back; ++j) {
+      if (result_cost[i] < result_cost[i - j]) {
         result_cost[i] = result_cost[i - j];
         column = i - j;
       }
@@ -34,15 +33,14 @@ int main(){
   }
   jumped_columns.push_back(column_count);
   cout << result_cost.back() << "\n";
-
   size_t col = jumped_columns.size() - 1;
   vector<size_t> pathway;
-  while (col > 0){
+  while (col > 0) {
     pathway.push_back(jumped_columns[col]);
     col = jumped_columns[col] - 1;
   }
   cout << pathway.size() - 1 << "\n";
-  for (size_t i = pathway.size() - 1; i > 0; --i){
+  for (size_t i = pathway.size() - 1; i > 0; --i) {
     cout << pathway[i] << " ";
   }
   cout << pathway[0];
