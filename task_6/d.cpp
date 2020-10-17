79ecdd2ecb020a6d15c0f2332c17e1b0fb28121e
IMDxD <nestor969@mail.ru>
2020-10-17 22:30:49 +0300

    fix task6 d.cpp codestyle

--- task_6/d.cpp
+++ task_6/d.cpp
@@ -9,23 +9,22 @@ using std::min;
 using std::string;
 using std::vector;
 
-
-int main(){
+int main() {
   string first;
   string second;
   cin >> first >> second;
   vector<vector<unsigned>> oper_matrix(first.size() + 1);
-  for (auto& v: oper_matrix){
+  for (auto& v: oper_matrix) {
     v.resize(second.size() + 1);
   }
   oper_matrix[0][0] = 0;
-  for (size_t j = 1; j <= second.size(); ++j){
+  for (size_t j = 1; j <= second.size(); ++j) {
     oper_matrix[0][j] = oper_matrix[0][j - 1] + 1;
   }
-  for (size_t i = 1; i <= first.size(); ++i){
+  for (size_t i = 1; i <= first.size(); ++i) {
     oper_matrix[i][0] = oper_matrix[i - 1][0] + 1;
-    for (size_t j = 1; j <= second.size(); ++j){
-      if (first[i - 1] == second[j - 1]){
+    for (size_t j = 1; j <= second.size(); ++j) {
+      if (first[i - 1] == second[j - 1]) {
         oper_matrix[i][j] = oper_matrix[i - 1][j - 1];
       } else {
         oper_matrix[i][j] = min(min(oper_matrix[i][j - 1],
