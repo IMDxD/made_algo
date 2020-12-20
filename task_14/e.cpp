561b971239eb3388b74ebe21934c6f73d8fa8027
IMDxD <nestor969@mail.ru>
2020-12-20 17:20:19 +0300

    task 14 e.cpp set intersection

--- task_14/e.cpp
+++ task_14/e.cpp
@@ -1,16 +1,18 @@
 #include "algorithm"
 #include "iostream"
+#include "set"
 #include "string"
 #include "vector"
 
 using std::cin;
 using std::cout;
 using std::min;
+using std::set;
 using std::string;
 using std::vector;
 
 const char FIRST_LETTER = 'a' - 1;
-const uint32_t POWER_CONST = 19;
+const uint32_t POWER_CONST = 29;
 const uint32_t MODULE = 2'000'003;
 const size_t MAX_LENGTH = 10'000;
 
@@ -45,36 +47,42 @@ int main() {
         }
         min_length = min(min_length, str_array[i].size());
     }
-    size_t left = 0;
-    size_t right = min_length + 1;
+    ssize_t left = 1;
+    ssize_t right = min_length + 1;
     string substr;
-    while (left < right - 1) {
-        size_t middle = (right + left) / 2;
-        bool is_exist = true;
-        for (size_t j = 0; j < str_array[0].size() - middle + 1; ++j) {
-            long long first_hash = get_hash(hash[0], power[0], j, j + middle - 1);
-            for (size_t i = 1; i < str_array.size(); ++i) {
-                bool has_substr = false;
-                for (size_t k = 0; k < str_array[i].size() - middle + 1; ++k) {
-                    if (get_hash(hash[i], power[i], k, k + middle - 1) == first_hash) {
-                        has_substr = true;
-                        break;
-                    }
-                }
-                if (!has_substr) {
-                    is_exist = false;
-                    break;
-                } else {
-                    is_exist = true;
+    while (left < right) {
+        bool is_exist = false;
+        ssize_t middle = (right + left) / 2;
+        set<long long> common_hash;
+        for (size_t i = 0; i < str_array[0].size() - middle + 1; ++i) {
+            common_hash.insert(get_hash(hash[0], power[0], i, i + middle - 1));
+        }
+        for (size_t i = 1; i < str_array.size() - 1; ++i) {
+            set<long long> tmp_hash;
+            for (size_t k = 0; k < str_array[i].size() - middle + 1; ++k) {
+                auto pos = common_hash.find(get_hash(hash[i], power[i], k, k + middle - 1));
+                if (pos != common_hash.end()) {
+                    tmp_hash.insert(*pos);
                 }
             }
-            if (is_exist) {
-                substr = str_array[0].substr(j, middle);
+            common_hash = tmp_hash;
+            if (common_hash.empty()) {
                 break;
             }
         }
+        if (!common_hash.empty()) {
+            size_t last = str_array.size() - 1;
+            for (size_t i = 0; i < str_array.back().size() - middle + 1; ++i) {
+                auto pos = common_hash.find(get_hash(hash[last], power[last], i, i + middle - 1));
+                if (pos != common_hash.end()) {
+                    substr = str_array.back().substr(i, middle);
+                    is_exist = true;
+                    break;
+                }
+            }
+        }
         if (is_exist) {
-            left = middle;
+            left = middle + 1;
         } else {
             right = middle;
         }
